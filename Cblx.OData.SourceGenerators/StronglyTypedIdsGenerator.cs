43b97727a570e30ca93cdee2fea4f0648b0779ab
Guilherme Meinlschmiedt Abdo <guilherme.abdo@codeblox.com.br>
2022-02-20 12:17:07 -0300

    add strongly typed id source generator

---
+++ Cblx.OData.SourceGenerators/StronglyTypedIdsGenerator.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Collections.Generic;
+namespace Cblx.OData.SourceGenerators
+{
+    [Generator]
+    public class StronglyTypedIdsGenerator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            var syntaxReceiver = context.SyntaxReceiver as SyntaxReceiver;
+            foreach (ClassDeclarationSyntax classDeclarationSyntax in syntaxReceiver.TbClasses)
+            {
+                SemanticModel model = context.Compilation.GetSemanticModel(classDeclarationSyntax.SyntaxTree);
+                var symbol = model.GetDeclaredSymbol(classDeclarationSyntax) as ITypeSymbol;
+                string name = $"{symbol.Name}Id";
+                string source = $@"using Cblx.OData.Client.Abstractions.Ids;
+using System.Text.Json.Serialization;
+namespace {symbol.ContainingNamespace};
+[JsonConverter(typeof(IdConverterFactory))]
+public partial record {name}(Guid Guid) : Id(Guid)
+{{
+    public static implicit operator Guid({name}? id) => id?.Guid ?? Guid.Empty;
+    public static implicit operator Guid?({name}? id) => id?.Guid;
+    public static implicit operator {name}(Guid guid) => new {name}(guid);
+}}";
+                context.AddSource($"{symbol.Name}Id.g.cs", source);
+            }
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            context.RegisterForSyntaxNotifications(() => new SyntaxReceiver());
+        }
+
+        class SyntaxReceiver : ISyntaxReceiver
+        {
+            public List<ClassDeclarationSyntax> TbClasses { get; } = new List<ClassDeclarationSyntax>();
+
+            public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+            {
+                if (syntaxNode is ClassDeclarationSyntax classDeclarationSyntax)
+                {
+                    if (classDeclarationSyntax.Identifier.ToString().StartsWith("Tb"))
+                    {
+                        TbClasses.Add(classDeclarationSyntax);
+                    }
+                }
+            }
+        }
+    }
+}
