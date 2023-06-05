748be20caa2fdff097eb4d426b2f587cc5ef31fb
Guilherme Meinlschmiedt Abdo <guilherme.abdo@codeblox.com.br>
2023-06-05 14:30:56 -0300

    🩹 detect other Flatten Attributes to check if is a Domain Entity Json Based

--- OData.Client.Abstractions/JsonTemplateHelper.cs
+++ OData.Client.Abstractions/JsonTemplateHelper.cs
@@ -14,15 +14,22 @@ internal static class JsonTemplateHelper
 
     /// <summary>
     /// Tells if a Domain Entity (for Repositories) can use the new mode based on it's JSON template (ex: when using FlattenJsonConverter).
+    /// This should be removed in the future, when the new SelectAndExpandParser and the new ChangeTracker are stable.
     /// </summary>
     /// <param name="type"></param>
     /// <returns></returns>
     public static bool IsJsonBasedDomainEntity(Type type)
     {
         return type.GetCustomAttributes()
-            .Any(attr => attr is JsonConverterAttribute jsonConverterAttribute &&
-            jsonConverterAttribute.ConverterType?.IsGenericType is true
-            && jsonConverterAttribute.ConverterType.GetGenericTypeDefinition() == typeof(FlattenJsonConverter<>));
+            .Any(attr => 
+            attr is FlattenRootAttribute
+            ||
+            attr is JsonConverterAttribute jsonConverterAttribute &&
+            (
+                (jsonConverterAttribute.ConverterType?.IsGenericType is true && jsonConverterAttribute.ConverterType.GetGenericTypeDefinition() == typeof(FlattenJsonConverter<>))
+                ||
+                jsonConverterAttribute.ConverterType == typeof(FlattenJsonConverterFactory)
+            ));
     }
 
     public static JsonObject GetTemplate<T>() => GetTemplate(typeof(T));
