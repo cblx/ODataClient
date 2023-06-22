ab4ecbe6572503899394923428176af114829e4b
Guilherme Meinlschmiedt Abdo <guilherme.abdo@codeblox.com.br>
2023-06-22 18:24:30 -0300

    👩🏻‍💻xt methods for better xp with repo/ChangeTracking

---
+++ Cblx.OData.Client.Repositories/EnumerableExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace Cblx.OData.Client;
+
+public static class EnumerableExtensions
+{
+    public static async Task<List<T>> AttachedTo<T>(this Task<List<T>> task, IChangeTracker changeTracker)
+    {
+        var list = await task;
+        return changeTracker.AttachOrGetCurrentRange(list).ToList();
+    }
+
+    public static async Task<T[]> AttachedTo<T>(this Task<T[]> task, IChangeTracker changeTracker)
+    {
+        var list = await task;
+        return changeTracker.AttachOrGetCurrentRange(list).ToArray();
+    }
+
+    public static async Task<T?> AttachedTo<T>(this Task<T?> task, IChangeTracker changeTracker)
+    {
+        var entity = await task;
+        return changeTracker.AttachOrGetCurrent(entity);
+    }
+}
